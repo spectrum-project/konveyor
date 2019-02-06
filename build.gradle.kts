64585a74a0db648cba68c55b1961b6c051f1401a
svok <svok@users.noreply.github.com>
2019-02-06 19:45:21 +0500

    Issue 2 (#9)
    Publicastion snapshot version in jfrog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 import com.jfrog.bintray.gradle.BintrayExtension
 import com.jfrog.bintray.gradle.tasks.BintrayUploadTask
-import java.util.*
-import java.time.*
+import groovy.lang.GroovyObject
+import org.jfrog.gradle.plugin.artifactory.dsl.DoubleDelegateWrapper
+import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
+import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 buildscript {
     repositories {
@@ -15,7 +18,7 @@ plugins {
     kotlin("multiplatform") version "1.3.20"
     `maven-publish`
     id("com.jfrog.bintray") version "1.8.4"
-//    id("com.jfrog.artifactory") version "4.9.0"
+    id("com.jfrog.artifactory") version "4.9.0"
     id("maven-publish")
     id("net.nemerosa.versioning") version "2.8.2"
     jacoco
@@ -115,7 +118,9 @@ kotlin {
 
                     attributes(
                         mutableMapOf(
-                            "Created-By" to "${System.getProperty("java.version")} (${System.getProperty("java.vendor")} ${System.getProperty("java.vm.version")})",
+                            "Created-By" to "${System.getProperty("java.version")} (${System.getProperty("java.vendor")} ${System.getProperty(
+                                "java.vm.version"
+                            )})",
                             "Built-By" to builtByValue,
                             "Build-Date" to buildDate,
                             "Build-Time" to buildTime,
@@ -212,39 +217,71 @@ kotlin {
                 }
             }
 
-            bintray {
-                user = System.getenv("bintrayUser")?.toString() ?: ""
-                key = System.getenv("bintrayKey")?.toString() ?: ""
-                override = true
-                setPublications("mavenJava")
+            val bintrayUsername = System.getenv("bintrayUser")?.toString() ?: ""
+            val bintrayApiKey = System.getenv("bintrayKey")?.toString() ?: ""
 
-                pkg(closureOf<BintrayExtension.PackageConfig> {
-                    repo = "konveyor"
-                    name = "konveyor"
-                    desc = "Conveyor belt software design pattern in a kotlin DSL-like style"
-                    userOrg = "spectrum-project"
-                    websiteUrl = "https://github.com/spectrum-project/konveyor"
-                    issueTrackerUrl = "https://github.com/spectrum-project/konveyor/issues"
-                    vcsUrl = "https://github.com/spectrum-project/konveyor.git"
-                    githubRepo = "spectrum-project/konveyor"
-                    githubReleaseNotesFile = "CHANGELOG.md"
-                    setLicenses("Apache-2.0")
-                    setLabels(
-                        "conveyor",
-                        "conveyor-belt",
-                        "processor",
-                        "workflow",
-                        "kotlin",
-                        "kotlin dsl",
-                        "handler"
-                    )
-                    publish = true
-                    setPublications("mavenJava")
-                    version(closureOf<BintrayExtension.VersionConfig> {
-                        this.name = project.version.toString()
-                        released = Date().toString()
+            if ((project.version as String).endsWith("-SNAPSHOT")) {
+                artifactory {
+                    setContextUrl("https://oss.jfrog.org/artifactory")
+                    //The base Artifactory URL if not overridden by the publisher/resolver
+                    publish(delegateClosureOf<PublisherConfig> {
+                        repository(delegateClosureOf<DoubleDelegateWrapper> {
+                            invokeMethod("setRepoKey", "oss-snapshot-local")
+                            invokeMethod("setUsername", bintrayUsername)
+                            invokeMethod("setPassword", bintrayApiKey)
+                            invokeMethod("setMavenCompatible", true)
+                            invokeMethod("setPublishBuildInfo", false)
+                        })
+
+                        defaults(delegateClosureOf<GroovyObject> {
+                            invokeMethod("publications", "mavenJava")
+                        })
                     })
-                })
+                }
+                publish {
+                    dependsOn(artifactoryDeploy)
+                    dependsOn(artifactoryPublish)
+                }
+            } else {
+
+                bintray {
+                    user = bintrayUsername
+                    key = bintrayApiKey
+                    override = true
+                    setPublications("mavenJava")
+
+                    pkg(closureOf<BintrayExtension.PackageConfig> {
+                        repo = "konveyor"
+                        name = "konveyor"
+                        desc = "Conveyor belt software design pattern in a kotlin DSL-like style"
+                        userOrg = "spectrum-project"
+                        websiteUrl = "https://github.com/spectrum-project/konveyor"
+                        issueTrackerUrl = "https://github.com/spectrum-project/konveyor/issues"
+                        vcsUrl = "https://github.com/spectrum-project/konveyor.git"
+                        githubRepo = "spectrum-project/konveyor"
+                        githubReleaseNotesFile = "CHANGELOG.md"
+                        setLicenses("Apache-2.0")
+                        setLabels(
+                            "conveyor",
+                            "conveyor-belt",
+                            "processor",
+                            "workflow",
+                            "kotlin",
+                            "kotlin dsl",
+                            "handler"
+                        )
+                        publish = true
+                        setPublications("mavenJava")
+                        version(closureOf<BintrayExtension.VersionConfig> {
+                            this.name = project.version.toString()
+                            released = Date().toString()
+                        })
+                    })
+                }
+                publish {
+                    dependsOn(bintrayUpload)
+                    dependsOn(bintrayPublish)
+                }
             }
 
             val bintrayUpload by existing(BintrayUploadTask::class) {
@@ -258,10 +295,6 @@ kotlin {
 //    named<Task>("afterReleaseBuild") {
 //        dependsOn(bintrayUpload)
 //    }
-            publish {
-                dependsOn(bintrayUpload)
-                dependsOn(bintrayPublish)
-            }
         }
     }
     //js()
